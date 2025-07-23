b7f5952d227a94157e15a07421e0a1a71317546f
lesage20 <angezanou00@gmail.com>
2025-07-23 22:04:00 +0000

    added qr-code features

--- app/(modal)/_layout.tsx
+++ app/(modal)/_layout.tsx
@@ -10,6 +10,9 @@ export default function ModalLayout() {
     >
       <Stack.Screen name="connection-detail" />
       <Stack.Screen name="connection-edit" />
+      <Stack.Screen name="scan-qr" />
+      <Stack.Screen name="my-qr" />
+      <Stack.Screen name="share-qr" />
     </Stack>
   );
 }
